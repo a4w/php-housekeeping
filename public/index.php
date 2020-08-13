390cbebedb79597e637bd16018c270cbc591c2cd
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2020-08-13 05:15:49 +0200

    Remove unused import

--- public/index.php
+++ public/index.php
@@ -5,7 +5,6 @@ include __DIR__ . './../vendor/autoload.php';
 use Housekeeping\Kernel;
 use Housekeeping\Routing\Router;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
 
 $router = new Router();
 
