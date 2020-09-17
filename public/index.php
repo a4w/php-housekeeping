26911226ed9c46843c08c78a70be7eb00535d51b
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2020-09-17 02:43:04 +0200

    Update index to match new router

--- public/index.php
+++ public/index.php
@@ -3,21 +3,15 @@
 include __DIR__ . './../vendor/autoload.php';
 
 use Housekeeping\Kernel;
-use Housekeeping\Routing\Router;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
-$router = new Router();
-
-$router->get('/', function () {
-    return "Server is running...";
-});
-
-$router->get('/hello', function () {
-    return "Hello world!";
-});
-
-$kernel = new Kernel(true, $router, function (Exception $exception) {
-    return new JsonResponse(['error' => true, 'message' => $exception->getMessage(), 'code' => $exception->getCode(), 'trace' => $exception->getTraceAsString()]);
-});
+$kernel = new Kernel(
+    /** Debug mode */
+    true,
+    /** Error handler */
+    function (Exception $exception) {
+        return new JsonResponse(['error' => true, 'message' => $exception->getMessage(), 'code' => $exception->getCode(), 'trace' => $exception->getTraceAsString()]);
+    }
+);
 
 $kernel->boot();
